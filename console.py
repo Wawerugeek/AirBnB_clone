387a716bfa1b244727cf3ff7e6393bed1a2d7f57
Wawerugeek <stevemuiruri84@gmail.com>
2023-05-13 01:38:50 +0300

    remember to remove excess comments

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the entry point of the command interpreter:"""
 import cmd 
+import shlex
 from models import storage
 from models.base_model import BaseModel
 import json
@@ -36,20 +37,21 @@ class HBNBCommand(cmd.Cmd):
             args (class name): the class name to create
         """
         if not args:
+            #if class name is missing, say its missing
             print("** class name missing **")
             return 
-        
-        try:
-            cls = eval(args)
-        
-        except NameError:
-            print ("** class doesn't exist ** ")
-            return 
-        
-        objs = cls()
-        storage.new(objs)
-        storage.save()
-        print(objs.id)
+        #split the arguments into a list 
+        my_list = shlex.split(args)
+
+        #check whether the class name is valid
+        if my_list[0] not in HBNBCommand.c_dict.keys():
+            print("** class doesn't exist **")
+            return
+        #create new instance
+        n_instance = HBNBCommand.c_dict[my_list[0]]()
+        #save new instance to Json file
+        n_instance.save()
+        print(n_instance.id)
 
     def do_show(self, args):
         """Prints the str rep of an instance based on cls name and ud
@@ -110,10 +112,81 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** no instance found **")
 
-    
-        
-        
+    def do_all(self, args):
+        """prints all string rep of all instances based or
+        not on the class name
 
+        Args:
+            args (_type_): _description_
+        """
+        #reload file from storage
+        storage.reload()
+
+        #get all objects from storage
+        objs_dict = storage.all()
+
+        #print all objects if no class name is provided
+        if not args:
+            objs_list = [str(obj) for obj in objs_dict.values()]
+            print(json.dumps(objs_list))
+        
+        else:
+            c_name = args.split()[0]
+            #check whether class exists
+            if c_name not in HBNBCommand.c_dict:
+                print("** class doesn't exist **")
+                return
+            objs_list = [str[obj] for key, obj in objs_dict.items if key.startswith(c_name) + '.']
+            print(json.dumps(objs_list))
+
+    def update(self, args):
+        """updates an instance based on the class name and id
+        by adding or updating attribute
+
+        Args:
+            args (_type_): update <class name> <id> <attribute name>
+            "<attribute value>"
+        """
+        if not args:
+            print("** class name missing **")
+            return 
+        #split the argument to handle quoted strings
+        my_list = shlex.split(args)
+
+        #ensure that the classname is valid
+        if my_list[0] not in HBNBCommand.c_dict.keys():
+            print("** class doesn't exist **")
+            return 
+        
+        #check whether instance id is provided
+        if len(my_list) < 2:
+            print("** instance id is missing **")
+            return 
+        
+        #retrieve an instance from storage 
+        objs_dict = storage.all()
+        key = f"{my_list[0]}.{my_list[1]}"
+        if key not in objs_dict:
+            print("** instance not found **")
+            return 
+
+        #ensure that attribute name and value are given
+        if len(my_list) == 2:
+            print("** attribute name missing **")
+            return 
+        
+        if len(my_list) == 3:
+            print("** value missing **")
+            return 
+        #check whether it has an instance and update
+        if hasattr(objs_dict[key], my_list[2]):
+            d_type = type(getattr(objs_dict[key], my_list[2]))
+            setattr(objs_dict[key], my_list[2], d_type(my_list[3]))
+        else:
+            setattr(objs_dict[key], my_list[2], my_list[3])
+
+        #save changes to storage
+        storage.save()
 
 if __name__=='__main__':
     HBNBCommand().cmdloop()
